7eff24239343f9696a592477777df4dbe0552fda
Florian Bourdenet <florian@attestis.com>
2025-03-24 20:50:11 +0100

    Remove comments

--- src/config.ts
+++ src/config.ts
@@ -6,11 +6,6 @@ dotenv.config();
 export const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
 export const DISCORD_CHANNEL_ID = process.env.DISCORD_CHANNEL_ID;
 
-/**
- * Configuration for fetching upcoming matches from Liquipedia's API.
- * Each entry corresponds to a specific team and game, with the API URL
- * to fetch upcoming matches for that team and game.
- */
 export const WATCHED_TEAMS_GAMES = [
   {
     team: "Karmine_Corp",
